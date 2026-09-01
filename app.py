1412c308c39779956f7185800137c25ac92185ac
quan26dec <49353022+quan26dec@users.noreply.github.com>
2026-09-01 20:48:57 +0900

    Update app.py

--- app.py
+++ app.py
@@ -15,7 +15,8 @@ else:
     st.error(f"❌ J-Quants 接続失敗：{response.status_code}")
 
 financial_url = "https://api.jquants.com/v2/fins/summary"
-financial_response = requests.get(financial_url, params={"code": "7751"}, headers=headers)
+stock_code = st.text_input("銘柄コード", value="7751")
+financial_response = requests.get(financial_url, params={"code": stock_code}, headers=headers)
 st.write("決算API ステータス:", financial_response.status_code)
 st.write("決算API 応答内容:", financial_response.text)
 
