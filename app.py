b78df36cffb3d49a38a4325524a5ad34b91846d4
quan26dec <49353022+quan26dec@users.noreply.github.com>
2026-09-03 20:59:47 +0900

    Update app.py

--- app.py
+++ app.py
@@ -232,7 +232,7 @@ if st.button("🚀 100銘柄をスクリーニング"):
 
         result = analyze_stock(code)
 
-        time.sleep(0.5)
+        time.sleep(1.0)
 
         if result is not None:
             results_50.append(result)
