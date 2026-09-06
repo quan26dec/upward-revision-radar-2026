88f217912bee337cebb0d2d5ea7b9ad660988d4a
quan26dec <49353022+quan26dec@users.noreply.github.com>
2026-09-06 14:55:13 +0900

    Update app.py

--- app.py
+++ app.py
@@ -659,13 +659,13 @@ if st.button("前年5日分を照合"):
                 "OPYoY"
             ]
         ].head(30)
-    )
-st.subheader("🧪 DocType確認")
-
-st.write(
-    calc_df["DocType_current"].value_counts()
-)
-    st.subheader("🧪 基準値・ThresholdGap 計算")
+            )
+        st.subheader("🧪 DocType確認")
+        
+        st.write(
+            calc_df["DocType_current"].value_counts()
+        )
+        st.subheader("🧪 基準値・ThresholdGap 計算")
 
     threshold_map = {
         "1Q": 35,
