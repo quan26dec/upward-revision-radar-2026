1a27395d0e401fa1ca74f2e0d89e5236bc79aab3
quan26dec <49353022+quan26dec@users.noreply.github.com>
2026-09-13 12:19:41 +0900

    Update mapping for recovery revision status

--- app.py
+++ app.py
@@ -1858,7 +1858,7 @@ if st.button("前年5日分を照合"):
     recovery_revision_result_df["今回修正ステータス"] = (
         recovery_revision_result_df["Code4"]
         .astype(str)
-        .map(revision_status_map)
+        .map(revision_all_status_map)
         .fillna("📡 今回修正なし")
     )
     
